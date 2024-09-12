9af9e3d129734f78d84c3733fe438744dad77f00
MaxxVoiid <113184699+maxxvoiid@users.noreply.github.com>
2024-09-12 18:21:07 -0300

    all scripts have the nerd message

--- scripts/playAsOpponent.lua
+++ scripts/playAsOpponent.lua
@@ -1,3 +1,19 @@
+--[[ Hey neds 🤓☝️
+
+Well, as the name says, this script handles the Play As Opponent
+This script works as it should, don't touch anything if you don't know what you are doing :)
+    ~ MaxxVoiid
+
+]]
+
+
+
+
+
+-----------------------------------------------------------------------
+    --- DON'T EDIT ANYTHING IF YOU DON'T KNOW WHAT YOU'RE DOING ---
+-----------------------------------------------------------------------
+
 local enabled = getModSetting('opponentplay')
 
 if enabled == true then
