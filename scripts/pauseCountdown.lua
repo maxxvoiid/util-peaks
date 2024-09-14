775314c12bda5ad3f2a08a86662c93cb28469cb3
MaxxVoiid <113184699+maxxvoiid@users.noreply.github.com>
2024-09-13 23:26:23 -0300

    utilEnabled Bug Fix on pauseCountdown.lua

--- scripts/pauseCountdown.lua
+++ scripts/pauseCountdown.lua
@@ -15,64 +15,72 @@ This script works as it should, don't touch anything if you don't know what you 
 -----------------------------------------------------------------------
 
 local resumeCountEnabled = getModSetting('resumecountenabled')
-local utilEnabled
+
+function onCreatePost()
+   closeIfUtilNotEnabled()
+end
 
 if resumeCountEnabled then
    local isON = false;
 
-   function onCreatePost()
-      if getVar("utilEnabled") == true then
-         utilEnabled = true
+   function onResume()
+      if not isON and curStep > 0 then
+         time = 3;
+         playSound('metronome')
+         openCustomSubstate('countdownOnResume', true);
       end
-   end
-
-   if utilEnabled then
-      function onResume()
-         if not isON and curStep > 0 then
-            time = 3;
-            playSound('metronome')
-            openCustomSubstate('countdownOnResume', true);
-         end
-         isON = false;
-     end
-     
-     function onCustomSubstateCreatePost(name)
-         if name == 'countdownOnResume' then
-            if not isON then
-               makeLuaText('countdownTxt', time, 0, 0, 0);
-               setTextSize('countdownTxt', 60);
-               setTextBorder('countdownTxt', 3, '000000')
-               screenCenter('countdownTxt', 'xy');
-               setObjectCamera("countdownTxt", "other")
-               addLuaText('countdownTxt', true);
-     
-               runTimer('pauseCountdown', 0.5);
-            end
-         end
-     end
-     
-     function onCustomSubstateUpdate(name, elapsed)
-         if name == 'countdownOnResume' then
-            if time <= 0 then
-               cancelTimer('pauseCountdown');
-               removeLuaText('countdownTxt');
-               isON = true;
-               closeCustomSubstate();
-            end
-         end
-     end
-     
-     function onTimerCompleted(tag)
-         if tag == 'pauseCountdown' then
-            time = time - 1;
-     
-            if time > 0 then
-              playSound('metronome');
-            end
-     
-            setTextString('countdownTxt', time);
+      isON = false;
+  end
+  
+  function onCustomSubstateCreatePost(name)
+      if name == 'countdownOnResume' then
+         if not isON then
+            makeLuaText('countdownTxt', time, 0, 0, 0);
+            setTextSize('countdownTxt', 60);
+            setTextBorder('countdownTxt', 3, '000000')
+            screenCenter('countdownTxt', 'xy');
+            setObjectCamera("countdownTxt", "other")
+            addLuaText('countdownTxt', true);
+  
             runTimer('pauseCountdown', 0.5);
          end
-     end
+      end
+  end
+  
+  function onCustomSubstateUpdate(name, elapsed)
+      if name == 'countdownOnResume' then
+         if time <= 0 then
+            cancelTimer('pauseCountdown');
+            removeLuaText('countdownTxt');
+            isON = true;
+            closeCustomSubstate();
+         end
+      end
+  end
+  
+  function onTimerCompleted(tag)
+      if tag == 'pauseCountdown' then
+         time = time - 1;
+  
+         if time > 0 then
+           playSound('metronome');
+         end
+  
+         setTextString('countdownTxt', time);
+         runTimer('pauseCountdown', 0.5);
+      end
+  end
+end
+
+function closeIfUtilNotEnabled()
+   local var, debug = getVar("utilEnabled"), getVar("utilLoadDebug")
+
+   if var == false or var == nil then
+      return close()
+   end
+
+   if debug == true and debug ~= nil then
+      local sName = 'PC'
+      debugPrint(sName..': OK')
    end
 end
